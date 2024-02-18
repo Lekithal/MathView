066c71f88dab0b954eef51b1ceaebde302c402fd
Lekithal <naturalmartian@gmail.com>
2024-02-18 22:03:35 +0800

    Started Something

---
+++ main.js
@@ -0,0 +1,24 @@
+class Integer {
+    constructor(value) {
+        this.value = value
+    }
+}
+
+class Decimal {
+    constructor(value) {
+        this.value = value
+    }
+}
+
+class Fraction {
+    constructor(numerator, denominator) {
+        this.numerator = numerator
+        this.denominator = denominator
+    }
+}
+
+class Operation {
+    constructor(operation) {
+        this.operation = operation
+    }
+}
