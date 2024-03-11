d06447536d2300006b267acc04b65d6f1e917e42
Lekithal <naturalmartian@gmail.com>
2024-03-11 10:49:41 +0800

    Operations

--- main.js
+++ main.js
@@ -1,24 +1,79 @@
-class Integer {
-    constructor(value) {
-        this.value = value
+class Fraction
+{
+    constructor(numerator, denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    negate()
+    {
+        return new Fraction(this.numerator * -1, this.denominator);
+    }
+
+    inverse()
+    {
+        return new Fraction(this.denominator, this.numerator)
     }
 }
 
-class Decimal {
-    constructor(value) {
-        this.value = value
+class Operation
+{
+    constructor(operation)
+    {
+        this.operation = operation;
     }
 }
 
-class Fraction {
-    constructor(numerator, denominator) {
-        this.numerator = numerator
-        this.denominator = denominator
+
+function addition(num1, num2)
+{
+    if(num1.constructor.name !== "Fraction" || num2.constructor.name !== "Fraction")
+    {
+        return false;
     }
+
+    let X1 = num1.numerator;
+    let X2 = num1.denominator;
+    
+    let Y1 = num2.numerator;
+    let Y2 = num2.denominator;
+
+    return new Fraction(X1 * Y2 + X2 * Y1, X2 * Y2);
 }
 
-class Operation {
-    constructor(operation) {
-        this.operation = operation
+function subtract(num1, num2)
+{
+    if(num1.constructor.name !== "Fraction" || num2.constructor.name !== "Fraction")
+    {
+        return false;
     }
+
+    return addition(num1, num2.negate());
+}
+
+function multiply(num1, num2)
+{
+    if(num1.constructor.name !== "Fraction" || num2.constructor.name !== "Fraction")
+    {
+        return false;
+    }
+
+    let X1 = num1.numerator
+    let X2 = num1.denominator
+    
+    let Y1 = num2.numerator
+    let Y2 = num2.denominator
+
+    return new Fraction(X1 * Y1, X2 * Y2)
+}
+
+function divide(num1, num2)
+{
+    if(num1.constructor.name !== "Fraction" || num2.constructor.name !== "Fraction")
+    {
+        return false;
+    }
+
+    return multiply(num2, num2.inverse())
 }
